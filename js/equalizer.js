3e49db568d084df4fca26f20ee5350e2a46a02c7
portalguitarshred <portalguitarshred@gmail.com>
2024-05-24 06:31:47 -0300

    Create equalizer.js
    JS do equalizador

---
+++ js/equalizer.js
@@ -0,0 +1,29 @@
+export class Equalizer {
+    constructor() {
+        this.bassControl = document.getElementById('bass');
+        this.midControl = document.getElementById('mid');
+        this.trebleControl = document.getElementById('treble');
+        this.init();
+    }
+
+    init() {
+        this.bassControl.addEventListener('input', (e) => this.adjustEqualizer('bass', e.target.value));
+        this.midControl.addEventListener('input', (e) => this.adjustEqualizer('mid', e.target.value));
+        this.trebleControl.addEventListener('input', (e) => this.adjustEqualizer('treble', e.target.value));
+        document.getElementById('resetEqualizer').addEventListener('click', () => this.resetEqualizer());
+    }
+
+    adjustEqualizer(type, value) {
+        console.log(`${type} set to ${value}`);
+        // Implementar lógica para ajustar o equalizador
+    }
+
+    resetEqualizer() {
+        this.bassControl.value = 0;
+        this.midControl.value = 0;
+        this.trebleControl.value = 0;
+        this.adjustEqualizer('bass', 0);
+        this.adjustEqualizer('mid', 0);
+        this.adjustEqualizer('treble', 0);
+    }
+}
