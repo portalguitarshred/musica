55f51415886f16eeb58197a5137a3f58bd9774e6
portalguitarshred <portalguitarshred@gmail.com>
2024-05-24 06:43:06 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 import './ui.js';
+import './equalizer.js';
+import './player.js';
+import './login.js';
+import './register.js';
+import './timer.js';
+
+// Inicialização do aplicativo, se necessário
+document.addEventListener('DOMContentLoaded', () => {
+    // Código de inicialização que depende dos módulos importados
+    console.log('Aplicativo iniciado');
+});
+
