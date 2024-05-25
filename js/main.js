780d2db483e7ce66ff656f4609645d5d894667f5
portalguitarshred <portalguitarshred@gmail.com>
2024-05-24 21:17:47 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,9 +5,7 @@ import './login.js';
 import './register.js';
 import './timer.js';
 
-// Inicialização do aplicativo, se necessário
 document.addEventListener('DOMContentLoaded', () => {
-    // Código de inicialização que depende dos módulos importados
+    // Inicialização do aplicativo, se necessário
     console.log('Aplicativo iniciado');
 });
-
